4ed47ddfbdb684ef34e967ccc031bb0069f835de
Erik "Bardez" Frantz <bardez@bardez.net>
2011-10-27 06:06:43 +0000

    Bringing solution up-to-date. XAudio2 managed wrapper implemented under DirectX project
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C52

---
+++ File Formats/External/RIFF/Wave/Enums/DataFormat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Bardez.Projects.InfinityPlus1.Files.External.RIFF.Wave.Enums
+{
+    /// <summary>Enmerator indicating the type of audio data (typically PCM)</summary>
+    public enum DataFormat : ushort /* UInt16 */
+    {
+        PCM = 1
+    }
+}
