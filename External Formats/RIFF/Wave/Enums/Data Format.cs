14fcf82d83ae44931172ded40b246f610ddc2f15
Erik "Bardez" Frantz <bardez@bardez.net>
2012-01-12 02:22:59 +0000

    Renamed namespaces from *.File to *.FileFormats
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C138

--- External Formats/RIFF/Wave/Enums/Data Format.cs
+++ External Formats/RIFF/Wave/Enums/Data Format.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Bardez.Projects.InfinityPlus1.Files.External.RIFF.Wave.Enums
+namespace Bardez.Projects.InfinityPlus1.FileFormats.External.RIFF.Wave.Enums
 {
     /// <summary>Enmerator indicating the type of audio data (typically PCM)</summary>
     public enum DataFormat : ushort /* UInt16 */
