7871c18401fd1d80950cc9d58d47778d25167af4
Erik "Bardez" Frantz <bardez@bardez.net>
2013-03-19 06:07:16 +0000

    Added in class to house the speaker positions & channel count in one reference
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C715

---
+++ Basic Structures/Win32/Audio/Speaker Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bardez.Projects.BasicStructures.Win32.Audio
+{
+    /// <summary>Represents a configuration of speakers</summary>
+    public class SpeakerConfiguration
+    {
+        #region Fields
+        /// <summary>Represents the positions of the speakers</summary>
+        public SpeakerPositions Positions { get; set; }
+
+        /// <summary>Represents the number of speakers represented</summary>
+        public UInt32 Count { get; set; }
+        #endregion
+    }
+}
