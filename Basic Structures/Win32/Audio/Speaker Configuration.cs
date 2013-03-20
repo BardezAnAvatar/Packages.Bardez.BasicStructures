d1b07635b892823d237f22163e9750e939d6e056
Erik "Bardez" Frantz <bardez@bardez.net>
2013-03-20 05:12:35 +0000

    Added in default & definition constructors
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C729

--- Basic Structures/Win32/Audio/Speaker Configuration.cs
+++ Basic Structures/Win32/Audio/Speaker Configuration.cs
@@ -12,5 +12,20 @@ namespace Bardez.Projects.BasicStructures.Win32.Audio
         /// <summary>Represents the number of speakers represented</summary>
         public UInt32 Count { get; set; }
         #endregion
+
+
+        #region Construction
+        /// <summary>Default constructor</summary>
+        public SpeakerConfiguration() { }
+
+        /// <summary>Definition constructor</summary>
+        /// <param name="channelCount">Count of speaker channels</param>
+        /// <param name="positions">Positioning of speaker channels</param>
+        public SpeakerConfiguration(UInt32 channelCount, SpeakerPositions positions)
+        {
+            this.Count = channelCount;
+            this.Positions = positions;
+        }
+        #endregion
     }
 }
