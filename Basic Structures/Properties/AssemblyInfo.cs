55ec2be0ecc0391f282b661c288d79054035b65d
Erik "Bardez" Frantz <bardez@bardez.net>
2013-12-13 07:45:56 +0000

    Updated the assembly information
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C823

--- Basic Structures/Properties/AssemblyInfo.cs
+++ Basic Structures/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Win32 Data Structures")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Basic Data Structures")]
+[assembly: AssemblyDescription("Basic Data Structures")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Win32 Data Structures")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("Erik \"Bardez\" Frantz")]
+[assembly: AssemblyProduct("Basic Data Structures")]
+[assembly: AssemblyCopyright("Copyright © Erik \"Bardez\" Frantz 2012 - 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
